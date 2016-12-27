b18c2c17805d039492a046232c56e36c59340f38
zhanghao639 <zhanghao639@pingan.com.cn>
2016-12-27 10:10:33 +0800

    fixed pagelet bug

--- process/pagelet.js
+++ process/pagelet.js
@@ -3,9 +3,10 @@
 module.exports = function(content, file, conf){
     var found = false, id = file.subpathNoExt.replace(/\//g, '_').replace('__bak__', '').replace('_', '');
 
-    content = '<code style="display: none;" id="' + id + '">' + content + '</code>'
+    content = '<textarea id="' + id + '">' + content.replace(/<\/textarea>/g, '<\\/textarea>') + '</textarea>'
             + '<script>(function(){'
             + 'var elem = document.getElementById("' + id + '");'
+            + 'elem.value = elem.value.replace(/<\\\\\\\/textarea>/g, \'</textarea>\');'
             + 'elem.removeAttribute("id");'
             + 'var nls = [/*PAGELET_ASYNCS_PLACEHOLDER:' + file.id + '*/];'
             + 'require.async([\'static/pagelet.js\'].concat(nls), function(Pagelet){'
